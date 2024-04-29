8a000e52b1d1a667f1ab561c7814ea6b99c98be1
shakedown3000 <annalaube90@gmail.com>
2024-04-29 14:57:35 +0200

    add new tasks

---
+++ src/01_basic_ts_1_1/basic.ts
@@ -0,0 +1,86 @@
+let studentName = 'Yoda';
+console.log(studentName);
+
+studentName = 'Luke';
+
+let jedi: boolean = true; 
+
+let parentName: string = 'Anakin';
+
+let yearsOfTraining: number = 5;
+
+let seventeen: number;
+seventeen = 17;
+
+const sum =  seventeen + 7;
+console.log(sum);
+
+
+// Addition
+let addition_operator: number = 1 + 1;
+console.log('addition: ' + addition_operator);
+
+// Subtraktion
+let subtraktion_operator: number = 2 - 1;
+console.log('subtraktion: ' + subtraktion_operator);
+
+// Multiplikation
+let multiplication_operator: number = 2 * 2;
+console.log('multiplication: ' + multiplication_operator);
+
+// Division
+let division_operator: number = 4 / 2;
+console.log('division: ' + division_operator);
+
+// Modulo: zeigt an, was der Rest sein würde.
+let modulo_operator: number = 14 % 5;
+console.log('modulus: ' + modulo_operator);
+
+let x= 20;
+let y = 30;
+console.log(x);
+console.log(y);
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+let z = 10;
+let resultOne = (x*y)/z;
+console.log (resultOne);
+
+let a = 15;
+let b = 9;
+
+console.log(a%b);
+
+let c = 20;
+let resultTwo = (a+b)*c;
+console.log(resultTwo);
+
+a++;
+console.log(a);
+b--;
+console.log(b);
+let resultThree = b-a;
+console.log(resultThree);
+console.log(resultOne%resultTwo);
+
+let score = (5 + 5) * 10;  
+console.log('Ergebnis: ' + score);
+
+let newscore = 0;  
+newscore = newscore + 10;  
+console.log('Ergebnis: ' + score);
+
+newscore += 10;  
+console.log('Ergebnis: ' + score);
+
+let zahl = 1;  
+zahl = zahl + 1;  
+zahl += 1;
+
+zahl++;  
+console.log('increment: ' + zahl);
+
+zahl--;  
+console.log('dekrement: ' + zahl);
